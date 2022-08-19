07ccbed627acd2eac3710ae31821fe8cb215c99b
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-08-19 19:03:10 +0530

    [DRV_I2C] Add demo for driver_i2c_eeprom application for PIC32CX_MTDB
    MH3-70445
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

---
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32cx_mtsh_db/initialization.c
@@ -0,0 +1,196 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+#pragma config SECURITY_BIT = CLEAR
+#pragma config BOOT_MODE = 0x3
+#pragma config PLANE_SELECTION = CLEAR
+#pragma config ERASE_FUNCTION_LOCK = 0x0
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_AT24 Initialization Data">
+
+/* I2C PLIB Interface Initialization for AT24 Driver */
+const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
+
+    /* I2C PLIB WriteRead function */
+    .writeRead = (DRV_AT24_PLIB_WRITE_READ)FLEXCOM3_TWI_WriteRead,
+
+    /* I2C PLIB Write function */
+    .write = (DRV_AT24_PLIB_WRITE)FLEXCOM3_TWI_Write,
+
+    /* I2C PLIB Read function */
+    .read = (DRV_AT24_PLIB_READ)FLEXCOM3_TWI_Read,
+
+    /* I2C PLIB Transfer Status function */
+    .isBusy = (DRV_AT24_PLIB_IS_BUSY)FLEXCOM3_TWI_IsBusy,
+
+    /* I2C PLIB Error Status function */
+    .errorGet = (DRV_AT24_PLIB_ERROR_GET)FLEXCOM3_TWI_ErrorGet,
+
+    /* I2C PLIB Callback Register */
+    .callbackRegister = (DRV_AT24_PLIB_CALLBACK_REGISTER)FLEXCOM3_TWI_CallbackRegister,
+};
+
+/* AT24 Driver Initialization Data */
+const DRV_AT24_INIT drvAT24InitData =
+{
+    /* I2C PLIB API  interface*/
+    .i2cPlib = &drvAT24PlibAPI,
+
+    /* 7-bit I2C Slave address */
+    .slaveAddress = 0x54,
+
+    /* EEPROM Page Size in bytes */
+    .pageSize = DRV_AT24_EEPROM_PAGE_SIZE,
+
+    /* Total size of the EEPROM in bytes */
+    .flashSize = DRV_AT24_EEPROM_FLASH_SIZE,
+
+    /* AT24 Number of clients */
+    .numClients = DRV_AT24_CLIENTS_NUMBER_IDX,
+
+    .blockStartAddress =    0x0,
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
+
+    SEFC0_Initialize();
+
+    SEFC1_Initialize();
+  
+    DWDT_Initialize();
+    CLK_Initialize();
+    RSTC_Initialize();
+    PIO_Initialize();
+
+
+
+	BSP_Initialize();
+    FLEXCOM3_TWI_Initialize();
+
+
+    sysObj.drvAT24 = DRV_AT24_Initialize(DRV_AT24_INDEX, (SYS_MODULE_INIT *)&drvAT24InitData);
+
+
+
+
+    APP_Initialize();
+
+
+    NVIC_Initialize();
+
+    /* MISRAC 2012 deviation block end */
+}
+
+
+/*******************************************************************************
+ End of File
+*/
