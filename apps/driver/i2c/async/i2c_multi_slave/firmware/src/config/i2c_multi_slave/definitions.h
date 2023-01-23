dc068c124f23c9947cb145bf3e3e61cadaa58faa
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-01-23 14:16:17 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_pic32cx_mtsh_db
    MH3-74129
    
    Signed-off-by: Jignesh Moradiya <Jignesh.Moradiya@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/i2c_multi_slave/definitions.h
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/i2c_multi_slave/definitions.h
@@ -54,6 +54,7 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/pio/plib_pio.h"
+#include "peripheral/supc/plib_supc.h"
 #include "peripheral/flexcom/twi/master/plib_flexcom3_twi_master.h"
 #include "peripheral/flexcom/usart/plib_flexcom0_usart.h"
 #include "peripheral/sefc/plib_sefc0.h"
