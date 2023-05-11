db914f1321fee26f947e2d0718124940b195a71d
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-11 17:56:06 +0530

    [DRV_SPI] Regenerated demo for driver_spi_eeprom_at25_at25_eeprom_read_write_pic32cx_mtsh_db
    MH3-75935

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/pic32cx_mtsh_db/initialization.c
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/pic32cx_mtsh_db/initialization.c
@@ -67,19 +67,23 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 // <editor-fold defaultstate="collapsed" desc="DRV_AT25 Initialization Data">
 
 /* SPI PLIB Interface Initialization for AT25 Driver */
-const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
+static const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
 
     /* SPI PLIB WriteRead function */
     .writeRead = (DRV_AT25_PLIB_WRITE_READ)FLEXCOM1_SPI_WriteRead,
 
     /* SPI PLIB Write function */
-    .write = (DRV_AT25_PLIB_WRITE)FLEXCOM1_SPI_Write,
+    .writeData = (DRV_AT25_PLIB_WRITE)FLEXCOM1_SPI_Write,
 
     /* SPI PLIB Read function */
-    .read = (DRV_AT25_PLIB_READ)FLEXCOM1_SPI_Read,
+    .readData = (DRV_AT25_PLIB_READ)FLEXCOM1_SPI_Read,
 
     /* SPI PLIB Transfer Status function */
     .isBusy = (DRV_AT25_PLIB_IS_BUSY)FLEXCOM1_SPI_IsBusy,
@@ -89,7 +93,7 @@ const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
 };
 
 /* AT25 Driver Initialization Data */
-const DRV_AT25_INIT drvAT25InitData =
+static const DRV_AT25_INIT drvAT25InitData =
 {
     /* SPI PLIB API  interface*/
     .spiPlib = &drvAT25PlibAPI,
@@ -113,6 +117,7 @@ const DRV_AT25_INIT drvAT25InitData =
 };
 
 // </editor-fold>
+
 
 
 // *****************************************************************************
@@ -144,7 +149,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -180,17 +185,26 @@ void SYS_Initialize ( void* data )
     FLEXCOM1_SPI_Initialize();
 
 
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
     sysObj.drvAT25 = DRV_AT25_Initialize(DRV_AT25_INDEX, (SYS_MODULE_INIT *)&drvAT25InitData);
 
 
 
 
+    /* MISRAC 2012 deviation block end */
     APP_Initialize();
 
 
     NVIC_Initialize();
 
+
     /* MISRAC 2012 deviation block end */
+
 }
 
 
