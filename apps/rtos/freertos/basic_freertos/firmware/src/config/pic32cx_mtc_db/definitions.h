0c5c6576bfa0d774cad205bd6a4d33c354c8a0c3
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-12 15:22:37 +0530

    [RTOS] Regenerated demo for rtos__freertos__basic_freertos__firmware__pic32cx_mtc_db.X
    MH3-102942

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cx_mtc_db/definitions.h
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cx_mtc_db/definitions.h
@@ -60,8 +60,8 @@
 #include "task.h"
 #include "peripheral/flexcom/usart/plib_flexcom0_usart.h"
 #include "peripheral/sefc/plib_sefc0.h"
-#include "osal/osal.h"
 #include "peripheral/sefc/plib_sefc1.h"
+#include "osal/osal.h"
 #include "task1.h"
 #include "task2.h"
 #include "task3.h"
@@ -78,13 +78,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CX2051MTC128"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX_MT"
-#define DEVICE_SERIES		 "PIC32CXMTC"
+#define DEVICE_NAME          "PIC32CX2051MTC128"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX_MT"
+#define DEVICE_SERIES        "PIC32CXMTC"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 200000000
+#define CPU_CLOCK_FREQUENCY 200000000U
 
 // *****************************************************************************
 // *****************************************************************************
