0c1fc318b6ca1b013c02b2424ea81a4af3d5d410
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-11-12 15:21:47 +0530

    [DRV_USART] Regenerated demo for driver__usart__sync__usart_echo__firmware__pic32cx_mtsh_db.X
    MH3-102932

--- apps/driver/usart/sync/usart_echo/firmware/src/config/pic32cx_mtsh_db/definitions.h
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/pic32cx_mtsh_db/definitions.h
@@ -61,11 +61,11 @@
 #include "task.h"
 #include "peripheral/flexcom/usart/plib_flexcom0_usart.h"
 #include "peripheral/sefc/plib_sefc0.h"
+#include "peripheral/sefc/plib_sefc1.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "peripheral/sefc/plib_sefc1.h"
 #include "app_usart_echo.h"
 
 
@@ -79,13 +79,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CX2051MTSH128"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX_MT"
-#define DEVICE_SERIES		 "PIC32CXMTSH"
+#define DEVICE_NAME          "PIC32CX2051MTSH128"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX_MT"
+#define DEVICE_SERIES        "PIC32CXMTSH"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 200000000
+#define CPU_CLOCK_FREQUENCY 200000000U
 
 // *****************************************************************************
 // *****************************************************************************
